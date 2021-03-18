7df20bc9c4c3f66829e20c135170f5d3e607b64a
Iangolana Riantsoa <riantsoaramelson@gmail.com>
2021-03-18 20:46:14 +0300

    add file accueil.js

---
+++ frontarchi/src/components/Accueil.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Accueil extends Component {
+    render() {
+        return (
+            <div>
+                <p>salut</p>
+            </div>
+        )
+    }
+}
+
+export default Accueil
